7a81b94abd60bad6b0e52f12f23f0a65f67c99db
amsibsam <amsibsam@gmail.com>
2016-06-23 14:03:00 +0700

    initial commit

---
+++ app/src/main/java/com/example/rahardyan/myapplication/ListAdapter.java
@@ -0,0 +1,8 @@
+package com.example.rahardyan.myapplication;
+
+/**
+ * Created by rahardyan on 23/06/16.
+ */
+public class ListAdapter {
+
+}
---
+++ app/src/main/java/com/example/rahardyan/myapplication/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.rahardyan.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    String[] data = {"a", "b", "c", "d"};
+    private ListView listView;
+    private ArrayAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.list_view);
+        adapter = new ArrayAdapter(MainActivity.this, R.layout.support_simple_spinner_dropdown_item, data);
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "ini item "+data[position], Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
